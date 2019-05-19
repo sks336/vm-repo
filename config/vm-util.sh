f7adf7ef006fed05480670193164a9db7046fa7f
$achin <sks336@gmail.com>
2019-05-19 17:18:56 +0530

    Create vm-util.sh

---
+++ config/vm-util.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+function makeHostAndBashEntries() {
+    IFS=
+    host_entries=$(curl -sS https://raw.githubusercontent.com/sks336/vm-repo/master/config/host_entries.txt)
+    echo ${host_entries} > /etc/hosts
+
+    bash_entries=$(curl -sS https://raw.githubusercontent.com/sks336/vm-repo/master/config/bash_entries.txt)
+    echo ${bash_entries} > /home/sachin/.bashrc
+}
+
+function pushIPInfoToConsul() {
+    VM_PREFIX=$1
+    NODE_ID=$2
+    IP_ADDR=$(ifconfig | grep -A 1 'eth1' | tail -1 | awk '{print $2}')
+    echo '>>>>> IP_ADDR='$IP_ADDR
+    DATA_TO_PUSH={\"hostname\":\""${VM_PREFIX}-${NODE_ID}"\",\""ipaddress"\":\""${IP_ADDR}"\"}
+    curl --request PUT --data $DATA_TO_PUSH http://vm-consul-vault-1:8500/v1/kv/ipaddress/${VM_PREFIX}/${NODE_ID}
+}
