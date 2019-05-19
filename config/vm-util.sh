40af10d86b9934f6813533ff1b298d1f00ee2c6a
$achin <sks336@gmail.com>
2019-05-19 18:00:25 +0530

    Update vm-util.sh

--- config/vm-util.sh
+++ config/vm-util.sh
@@ -16,5 +16,5 @@ function pushIPInfoToConsul() {
     IP_ADDR=$(ifconfig | grep -A 1 'eth1' | tail -1 | awk '{print $2}')
     echo '>>>>> IP_ADDR='$IP_ADDR
     DATA_TO_PUSH={\"hostname\":\""${VM_PREFIX}-${NODE_ID}"\",\""ipaddress"\":\""${IP_ADDR}"\"}
-    curl --request PUT --data $DATA_TO_PUSH http://vm-consul-vault-1:8500/v1/kv/ipaddress/${VM_PREFIX}/${NODE_ID}
+    curl --request PUT --data $DATA_TO_PUSH http://192.168.109.11:8500/v1/kv/ipaddress/${VM_PREFIX}/${NODE_ID}
 }
