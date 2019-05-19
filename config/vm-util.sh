6762ddbaa8d1bd2bad717fe17bd826547cdb9aef
$achin <sks336@gmail.com>
2019-05-19 20:18:49 +0530

    Update vm-util.sh

--- config/vm-util.sh
+++ config/vm-util.sh
@@ -20,3 +20,46 @@ function pushIPInfoToConsul() {
     echo $DATA_TO_PUSH
     curl --request PUT --data $DATA_TO_PUSH http://${CONSUL_IP}:8500/v1/kv/ipaddress/${VM_PREFIX}/${NODE_ID}
 }
+
+function refreshIPsFromConsul() {
+echo '127.0.0.1 localhost
+127.0.0.1   vm-sachin
+255.255.255.255 broadcasthost
+::1             localhost
+
+192.168.0.120   vm-0
+192.168.109.11  vm-consul-vault-1
+192.168.109.12  vm-consul-vault-2
+192.168.109.13  vm-consul-vault-3
+
+########################################
+
+'>/etc/hosts
+
+UPDATE_FROM_CONSUL="false"
+
+while getopts c opts
+do
+    case "${opts}" in
+        c) echo 'Option "-c" is chosen, Going to update from Consul.....'
+            UPDATE_FROM_CONSUL="true";;
+    esac
+done
+
+if [ "$UPDATE_FROM_CONSUL" == "true" ]; then
+        CONSUL_IP=192.168.109.11
+        nKeys=$(curl -sS http://${CONSUL_IP}:8500/v1/kv/?keys | jq length)
+        for ((i=0;i<nKeys;i++)); do
+                key=$(curl -sS http://${CONSUL_IP}:8500/v1/kv/?keys | jq -r .[$i])
+                json=$(curl -sS http://${CONSUL_IP}:8500/v1/kv/$key | jq -r .[0].Value | base64 --decode)
+                host=$(echo $json | jq -r .hostname)
+                ip=$(echo $json | jq -r .ipaddress)
+                echo -e "$ip   $host" >> /private/etc/hosts
+        done
+
+fi
+
+echo 'Host Entry cleaned....'
+cat /etc/hosts
+
+}
