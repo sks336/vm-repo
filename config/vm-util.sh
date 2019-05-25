3078f7d092e90319e22e3769aab7367ea3bc49ca
$achin <sks336@gmail.com>
2019-05-25 15:41:27 +0530

    Update vm-util.sh

--- config/vm-util.sh
+++ config/vm-util.sh
@@ -79,7 +79,7 @@ function registerToConsul() {
     NODE_TYPE=$2
     rm -rf ${HOME}/consul
     mkdir -p ${HOME}/consul
-    cp -rf ${RESOURCES_DIR}/consul/* ${HOME}/consul
+    cp -rf ${RESOURCES_DIR}/config/consul/* ${HOME}/consul
     IP_ADDR=$(ifconfig | grep -A 3 'eth1' | grep inet | grep netmask | awk '{print $2}')
     find ${HOME}/consul/service -type f | xargs sed -i  "s/<ID>/${NODE_ID}/g"
     find ${HOME}/consul/service -type f | xargs sed -i  "s/<NODE_ID>/${IP_ADDR}/g"
